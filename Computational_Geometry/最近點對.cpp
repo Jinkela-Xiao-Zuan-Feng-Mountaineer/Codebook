9a3b569183d60146f75d17ffc12803e9e7e9bb67
jacky860226 <jacky860226@yahoo.com.tw>
2017-05-09 01:18:11 +0800

    update 20 codebook

--- Computational_Geometry/最近點對.cpp
+++ Computational_Geometry/最近點對.cpp
@@ -1,4 +1,4 @@
-#define INF LLONG_MAX/*預設是long long最大值*/
+#define INF LLONG_MAX
 template<typename T>
 T closest_pair(vector<point<T> >&v,vector<point<T> >&t,int l,int r){
 	T dis=INF,tmd;
@@ -9,7 +9,7 @@ T closest_pair(vector<point<T> >&v,vector<point<T> >&t,int l,int r){
 	t.clear();
 	for(int i=l;i<=r;++i)
 		if((v[i].x-v[mid].x)*(v[i].x-v[mid].x)<dis)t.push_back(v[i]);
-	sort(t.begin(),t.end(),point<T>::y_cmp);/*如果用merge_sort的方式可以O(n)*/
+	sort(t.begin(),t.end(),point<T>::y_cmp);//如果用merge_sort的方式可以O(n)
 	for(int i=0;i<(int)t.size();++i)
 		for(int j=1;j<=3&&i+j<(int)t.size();++j)
 			if((tmd=(t[i]-t[i+j]).abs2())<dis)dis=tmd;
@@ -19,5 +19,5 @@ template<typename T>
 inline T closest_pair(vector<point<T> > &v){
 	vector<point<T> >t;
 	sort(v.begin(),v.end(),point<T>::x_cmp);
-	return closest_pair(v,t,0,v.size()-1);/*最近點對距離*/
+	return closest_pair(v,t,0,v.size()-1);//最近點對距離
 }
