ad43c4e249413332330e7c4986ffa6054ca769d9
104000045 <jacky860226@yahoo.com.tw>
2018-05-22 11:09:36 +0800

    SA、LCP縮行數

--- String/suffix_array_lcp.cpp
+++ String/suffix_array_lcp.cpp
@@ -4,18 +4,20 @@
 	for(i=1;i<A;++i)c[i]+=c[i-1];\
 	for(i=n-1;~i;--i)sa[--c[x[y[i]]]]=y[i];\
 }
-#define sac(r,a,b) r[a]!=r[b]||a+k>=n||r[a+k]!=r[b+k]
+#define AC(r,a,b)\
+	r[a]!=r[b]||a+k>=n||r[a+k]!=r[b+k]
 void suffix_array(const char *s,int n,int *sa,int *rank,int *tmp,int *c){
 	int A='z'+1,i,k,id=0;
 	for(i=0;i<n;++i)rank[tmp[i]=i]=s[i];
 	radix_sort(rank,tmp);
 	for(k=1;id<n-1;k<<=1){
 		for(id=0,i=n-k;i<n;++i)tmp[id++]=i;
-		for(i=0;i<n;++i)if(sa[i]>=k)tmp[id++]=sa[i]-k;
+		for(i=0;i<n;++i)
+			if(sa[i]>=k)tmp[id++]=sa[i]-k;
 		radix_sort(rank,tmp);
 		swap(rank,tmp);
 		for(rank[sa[0]]=id=0,i=1;i<n;++i)
-			rank[sa[i]]=id+=sac(tmp,sa[i-1],sa[i]);
+			rank[sa[i]]=id+=AC(tmp,sa[i-1],sa[i]);
 		A=id+1;
 	}
 }
@@ -28,5 +30,5 @@ void suffix_array_lcp(const char *s,int len,int *h,int *sa,int *rank){
 		while(s[i+k]==s[sa[rank[i]-1]+k])++k;
 		h[rank[i]]=k;
 	}
-	h[0]=0;
+	h[0]=0;// h[k]=lcp(sa[k],sa[k-1]);
 }
