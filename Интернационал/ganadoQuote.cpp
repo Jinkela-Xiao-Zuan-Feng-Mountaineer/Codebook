77c7affe54bc0a5c33ec3e7927770abeea04f41d
104000045 <jacky860226@yahoo.com.tw>
2018-09-04 15:26:47 +0800

    Las Plagas

---
+++ Интернационал/ganadoQuote.cpp
@@ -0,0 +1,71 @@
+¡Allí está!
+¡Un forastero!
+¡Agarrenlo!
+¡Os voy a romper a pedazos!
+¡Cógelo!
+¡Te voy a hacer picadillo!
+¡Te voy a matar!
+¡Míralo, está herido!
+¡Sos cerdo!
+¿Dónde estás?
+¡Detrás de tí, imbécil!
+¡No dejes que se escape!
+¡Basta, hijo de puta!
+Lord Saddler...
+
+¡Mátalo!
+¡Allí está!
+Morir es vivir.
+Sííííí, ¡Quiero matar!
+Muere, muere, muere....
+Cerebros,cerebros,cerebros...
+Cógedlo, cógedlo, cógedlo...
+Lord Saddler...
+Dieciséis.
+
+¡Va por él!
+¡Muérete!
+¡Cógelo!
+¡Te voy a matar!
+¡Bloqueale el paso!
+¡Te cogí!
+¡No dejes que se escape!
+
+¿Qué carajo estás haciendo aquí? ¡Lárgate, cabrón!
+Hay un rumor de que hay un extranjero entre nosotros.
+Nuestro jefe se encargará de la rata.
+Su "Las Plagas" es mucho mejor que la nuestra.
+Tienes razón, es un hombre.
+Usa los músculos.
+Se vuelve loco!
+¡Hey, acá!
+¡Por aquí!
+¡El Gigante!
+¡Del Lago!
+¡Cógelo!
+¡Cógenlo!
+¡Allí!
+¡Rápido!
+¡Empieza a rezar!
+¡Mátenlos!
+¡Te voy a romper en pedazos!
+¡La campana!
+Ya es hora de rezar.
+Tenemos que irnos.
+¡Maldita sea, mierda!
+¡Ya es hora de aplastar!
+¡Mierda!
+¡Puedes correr, pero no te puedes esconder!
+¡Sos cerdo!
+¡Está en la trampa!
+¡Ah, que madre!
+¡Vámonos!
+¡Ándale!
+¡Cabrón!
+¡Coño!
+¡Agárrenlo!
+Cógerlo, Cógerlo...
+¡Allí está, mátalo!
+¡No dejas que se escape de la isla vivo!
+¡Hasta luego!
+¡Rápido, es un intruso!
