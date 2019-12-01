a6e5d33113920dad865333ec9a351e726e21720a
LFsWang <7088579+LFsWang@users.noreply.github.com>
2019-12-01 16:52:08 +0800

    Update Lucas.cpp
    shorter version

--- Number_Theory/Lucas.cpp
+++ Number_Theory/Lucas.cpp
@@ -1,16 +1,8 @@
-int mod_fact(int n,int &e){
-	e=0;
-	if(n==0)return 1;
-	int res=mod_fact(n/P,e);
-	e += n/P; 
-	if((n/P)%2==0)return res*fact[n%P]%P;
-	return res*(P-fact[n%P])%P;
+ll C(ll n, ll m, ll p){// n!/m!/(n-m)!
+ if(n<m) return 0;
+ return f[n]*inv(f[m],p)%p*inv(f[n-m],p)%p;
 }
-int Cmod(int n,int m){
-	int a1,a2,a3,e1,e2,e3;
-	a1=mod_fact(n,e1);
-	a2=mod_fact(m,e2);
-	a3=mod_fact(n-m,e3);
-	if(e1>e2+e3)return 0;
-	return a1*inv(a2*a3%P,P)%P;
+ll L(ll n, ll m, ll p){
+ if(!m) return 1;
+ return C(n%p,m%p,p)*L(n/p,m/p,p)%p;
 }
