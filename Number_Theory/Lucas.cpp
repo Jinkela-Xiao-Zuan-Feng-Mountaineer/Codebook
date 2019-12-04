2c194eaedab92b7a4b3838f8b9f33e71b4a535e3
LFsWang <7088579+LFsWang@users.noreply.github.com>
2019-12-04 13:56:08 +0800

    Update Lucas.cpp
    add Wilson

--- Number_Theory/Lucas.cpp
+++ Number_Theory/Lucas.cpp
@@ -6,3 +6,9 @@ ll L(ll n, ll m, ll p){
  if(!m) return 1;
  return C(n%p,m%p,p)*L(n/p,m/p,p)%p;
 }
+ll Wilson(ll n, ll p){ // n!%p
+	if(!n)return 1; 
+	ll res=Wilson(n/p, p);  
+	if((n/p)%2) return res*(p-f[n%p])%p;	
+	return  res*f[n%p]%p; //(p-1)!%p=-1
+}
